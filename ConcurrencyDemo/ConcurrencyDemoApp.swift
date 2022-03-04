a01ca4f95a887e824d1eeb5e240571c959e44223
Eric Cheung <eric1162004@hotmail.com>
2022-03-04 12:00:55 -0800

    Initial Commit

---
+++ ConcurrencyDemo/ConcurrencyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyDemoApp.swift
+//  ConcurrencyDemo
+//
+//  Created by eric on 2022-03-04.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
